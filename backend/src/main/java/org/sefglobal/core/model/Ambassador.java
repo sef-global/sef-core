75718735765d4f03205c813bdcdd10959c6017b9
piumal1999 <piumalrathnayake@hotmail.com>
2020-05-24 22:22:16 +0530

    Set status using Status enum

--- backend/src/main/java/org/sefglobal/core/model/Ambassador.java
+++ backend/src/main/java/org/sefglobal/core/model/Ambassador.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
+import org.sefglobal.core.util.Status;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
@@ -33,7 +34,7 @@ public class Ambassador extends AuditModel {
 
     @NotNull
     @Column(name = "status", length = 10)
-    private String status = "ACTIVE";
+    private String status = Status.ACTIVE.toString();
 
     public long getId() {
         return id;
--- backend/src/main/java/org/sefglobal/core/model/Event.java
+++ backend/src/main/java/org/sefglobal/core/model/Event.java
@@ -3,6 +3,7 @@ package org.sefglobal.core.model;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
+import org.sefglobal.core.util.Status;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
@@ -29,7 +30,7 @@ public class Event extends AuditModel {
 
     @NotNull
     @Column(length = 10)
-    private String status = "ACTIVE";
+    private String status = Status.ACTIVE.toString();
 
     public String getLink() {
         return link;
