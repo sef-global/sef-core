e0c21c4a60ea310aefced559bff2a4ffb890132d
Gravewalker666 <45477334+Gravewalker666@users.noreply.github.com>
2020-06-07 12:45:22 +0530

    Make academix endpoints public (#47)

--- backend/src/main/java/org/sefglobal/core/config/SecurityConfig.java
+++ backend/src/main/java/org/sefglobal/core/config/SecurityConfig.java
@@ -43,6 +43,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf().disable()
                 .authorizeRequests()
                     .antMatchers("/link/*").permitAll()
+                    .antMatchers("/academix/*").permitAll()
                     .anyRequest().authenticated()
                 .and()
                     .formLogin()
